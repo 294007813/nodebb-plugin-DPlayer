55963065b5e2150b91b67076d09473b207b435c3
lanto <lanto@lantodeMacBook.local>
2018-05-18 16:59:12 +0800

    0.0.2

--- static/client.js
+++ static/client.js
@@ -8,17 +8,32 @@ $(document).ready(function() {
 	$(window).on('action:ajaxify.end', function(ev, data) {
 		$('[data-toggle="tooltip"]').tooltip();
 
+	});
+
+	$(window).on('action:topic.loaded', function(ev, data) {
 		require(['DPlayer'], function (DPlayer) {
 			$(".dplayer-block").each(function (index,element) {
-				new DPlayer({
-					container: element,
-					video: {
-						url: $(element).children("a").attr("href"),
-					},
-				});
+				var address=''
+				if($(element).children("a").length>0){
+					address = $(element).children("a").attr("href")
+					console.log(address)
+					var video= new DPlayer({
+						container: element,
+						video: {
+							url: address,
+						},
+					});
+					document.addEventListener("clickPlay",function(e){
+						if(video.video.src.indexOf('/login')>=0){
+							window.location.href='/login'
+						}
+					})
+				}
+
 			})
 		});
 	});
+
 
 	$(window).on('action:composer.enhanced', function(evt, data) {
 		VideoExtendedMarkdown.prepareFormattingTools();
@@ -37,13 +52,13 @@ $(document).ready(function() {
 			if (formatting && controls) {
 				// params is (language, namespace, callback)
 				translator.getTranslations(window.config.userLang || window.config.defaultLang, 'videoextendedmarkdown', function(strings) {
-					formatting.addButtonDispatch('center', function(textarea, selectionStart, selectionEnd) {
+					formatting.addButtonDispatch('video', function(textarea, selectionStart, selectionEnd) {
 						if (selectionStart === selectionEnd) {
-							controls.insertIntoTextarea(textarea, '!(' + strings.video + ')');
-							controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.video.length);
+							controls.insertIntoTextarea(textarea, '!(' + strings.video_text + ')');
+							controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.video_text.length);
 						} else {
 							var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '!(', ')');
-							controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 1);
+							controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
 						}
 					});
 				})
